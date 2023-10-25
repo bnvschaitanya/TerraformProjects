454212f4aa5d67c5334eed910b1bc06032257d9a
Bellamkonda Narendra Venkata Sai Chaitanya <104776975+bnvschaitanya@users.noreply.github.com>
2023-10-25 19:51:25 +0530

    Update userdata.sh

--- EC2_VPC/userdata.sh
+++ EC2_VPC/userdata.sh
@@ -32,7 +32,7 @@ cat <<EOF > /var/www/html/index.html
 <body>
   <h1>Terraform Project Server 1</h1>
   <h2>Instance ID: <span style="color:green">$INSTANCE_ID</span></h2>
-  <p>Welcome to Abhishek Veeramalla's Channel</p>
+  <p>Welcome to B N V S Chaitanya's Website</p>
   
 </body>
 </html>
